ffa9d53718e08c13fb3605b2d2fc2da855a8f86f
aru <firethedancannon@gmail.com>
2021-08-04 13:32:45 -0400

    a

---
+++ config.sh
@@ -0,0 +1,2 @@
+rm ~/.bashrc ~/.vim/ ~/.vimrc -r
+cp ~/global-configs/.bashrc ~/global-configs/.vimrc ~/global-configs/.vim ~ -r
---
+++ replace.sh
@@ -0,0 +1,3 @@
+rm ~/global-configs/.bashrc ~/global-configs/.vim/ ~/global-configs/.vimrc -r
+cp ~/.bashrc ~/.vimrc ~/.vim/ ~/global-configs/ -r
+rm ~/global-configs/.vim/.netrwhist
